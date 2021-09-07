1caf3b44d32c15e59dc2ccb7a9e066e1819eb68f
whoant <vovanhoangtuan4.2@gmail.com>
2021-09-07 23:08:36 +0700

    Done

---
+++ index.js
@@ -0,0 +1,41 @@
+const callApi = require('./lib');
+
+class Nhaccuatui {
+    getMediaInfo(key) {
+        return callApi.post('/media/info', {
+            key,
+            type: 'playlist',
+        });
+    }
+
+    getLyric(key) {
+        return callApi.post('/lyric', {
+            key,
+            type: 'song',
+        });
+    }
+
+    getTop20() {
+        return callApi.post('/ranking/top20', {
+            category: 'nhac-viet',
+            type: 'song',
+            size: 20,
+        });
+    }
+
+    getTop100(key) {
+        return callApi.post('/top100', {
+            key,
+        });
+    }
+
+    getHome() {
+        return callApi.post('/home');
+    }
+
+    getTopic() {
+        return callApi.post('/topic');
+    }
+}
+
+module.exports = new Nhaccuatui();
---
+++ lib.js
@@ -0,0 +1,53 @@
+const axios = require('axios');
+const cryptoJS = require('crypto-js');
+const queryString = require('querystring');
+
+const KEY =
+    'U2FsdGVkX19BWoF1uTP8o90p9KAWsXZ/VJ41PG7XYF/63qnjiMh1TLy8zAfZBMa9iqiGyPiN5iMUocpD74kAsg==';
+
+const API_KEY = 'e3afd4b6c89147258a56a641af16cc79';
+
+const encrypt = () => {
+    const t = new Date().getTime().toString();
+
+    const n = cryptoJS.AES.decrypt(KEY, 'nhaccuatui').toString(
+        cryptoJS.enc.Utf8
+    );
+    const s = cryptoJS.HmacSHA512(t, n).toString();
+    return {
+        a: API_KEY,
+        s,
+        t,
+    };
+};
+
+const instance = axios.create({
+    baseURL: 'https://beta.nhaccuatui.com/api',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    },
+});
+
+instance.interceptors.request.use(
+    function (config) {
+        config.url += `?${queryString.stringify(encrypt())}`;
+        config.data = queryString.stringify(config.data);
+
+        return config;
+    },
+    function (error) {
+        return Promise.reject(error);
+    }
+);
+
+// Add a response interceptor
+instance.interceptors.response.use(
+    function (response) {
+        return response.data;
+    },
+    function (error) {
+        return Promise.reject(error);
+    }
+);
+
+module.exports = instance;
